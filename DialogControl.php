b76cdb0d3c98d04b3c7d085ddbcf2c209c179696
Petr Kessler <kesspess@gmail.com>
2012-10-05 14:05:40 +0200

    init commit

---
+++ DialogControl.php
@@ -0,0 +1,46 @@
+<?php
+
+use Nette\Application\UI;
+
+
+class DialogControl extends BaseControl
+{
+	/** @var string */
+	protected $message = NULL;
+
+	/** @var UI\Form */
+	protected $form = NULL;
+
+
+
+	function setup($message, UI\Form $form)
+	{
+		$this->message = $message;
+		$this->form = $form;
+		$this->invalidateControl();
+
+		return $this;
+	}
+
+
+
+	function reset()
+	{
+		$this->message = $this->form = NULL;
+		$this->invalidateControl();
+
+		return $this;
+	}
+
+
+
+	function render()
+	{
+		if ($this->message !== NULL && $this->form !== NULL) {
+			$this->template->message = $this->message;
+			$this->template->form = $this->form;
+		}
+
+		$this->template->render();
+	}
+}
